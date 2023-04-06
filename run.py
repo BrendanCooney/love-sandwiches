79e5a449632ddce659c0475ee2c4314e454a113e
BrendanCooney <advicast@gmail.com>
2023-04-06 23:51:19 +0000

    collect sales data for last 5 markets in columns

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ def validate_data(values):
         return False
 
     return True
-    
+
 
 def update_worksheet(data, worksheet):
     """
@@ -83,6 +83,17 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    This function calcualtes the las 5 entries sales. 
+    """
+    sales = SHEET.worksheet("sales")
+ 
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns    
 
 def main():
     """
@@ -97,4 +108,6 @@ def main():
     update_worksheet(new_surplus_data,"surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
